974edcf0db78fab9b10198f55cdc45c441980cca
kacho65535 <61747670+kacho65535@users.noreply.github.com>
2020-03-15 18:07:09 +0900

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,17 +1,18 @@
 struct edge
 {
-    ll to;
-    ll cost;
+    long long to;
+    long long cost;
 };
-typedef pair<ll, ll> P; //P.first:最短距離,P.second:頂点の番号
+typedef pair<long long, long long> P; //P.first:最短距離,P.second:頂点の番号
 //頂点V個の負閉路なし重み付きグラフGにおける始点sからの最短距離を求める
-ll V;
+long long V;
 vector<vector<edge>> G;
-void dijkstra(vec &dist, ll s)
+void dijkstra(vector<long long> &dist, long long s)
 {
     //greater<P>によってfirstが小さい順に取り出される
     priority_queue<P, vector<P>, greater<P>> que;
-    loop(i, V) dist[i] = 1e18;
+    for (long long i = 0; i < V; i++)
+        dist[i] = 1e18;
     dist[s] = 0;
     que.push(P(0, s));
     //queが空になるまで繰り返す
@@ -19,10 +20,10 @@ void dijkstra(vec &dist, ll s)
     {
         P p = que.top();
         que.pop();
-        ll v = p.second;
+        long long v = p.second;
         if (dist[v] < p.first)
             continue; //すでに最短距離ならスルー
-        loop(i, G[v].size())
+        for (long long i = 0; i < G[v].size(); i++)
         {
             edge e = G[v][i];
             //更新作業
