4c412aede1a9a16992fa271dfb1abe704dafc6b1
kacho65535 <61747670+kacho65535@users.noreply.github.com>
2020-03-15 15:14:52 +0900

    Update warshall_floyd.cpp

--- warshall_floyd.cpp
+++ warshall_floyd.cpp
@@ -1,9 +1,24 @@
 /*
 全頂点対最短距離を求める
+※必ずwatshall_floyd_init(V, dist);を記述した後入力を受け取らないとバグります...
 dist[i][i]<0なら負閉路あり
 dist[i][j]==1e18ならi->jの経路を持たない
 計算量O(V^3)
 */
+void watshall_floyd_init(long long V, vector<vector<long long>> &dist)
+{
+    //初期化
+    for (long long i = 0; i < V; i++)
+    {
+        for (long long j = 0; j < V; j++)
+        {
+            if (i == j)
+                dist[i].push_back(0);
+            else
+                dist[i].push_back(1e18);
+        }
+    }
+}
 void warshall_floyd(long long V, vector<vector<long long>> &dist)
 {
     for (long long k = 0; k < V; k++)
@@ -18,23 +33,14 @@ void warshall_floyd(long long V, vector<vector<long long>> &dist)
         }
     }
 }
-//以下Main関数における例
+//以下Main関数内における例
 int main()
 {
     long long V, E; //V:頂点数,E:辺数
     cin >> V >> E;
     vector<vector<long long>> dist(V);
-    //初期化
-    for (long long i = 0; i < V; i++)
-    {
-        for (long long j = 0; j < V; j++)
-        {
-            if (i == j)
-                dist[i].push_back(0);
-            else
-                dist[i].push_back(1e18);
-        }
-    }
+    //以下必須!
+    watshall_floyd_init(V, dist);
     //入力受け取り
     for (long long i = 0; i < E; i++)
     {
